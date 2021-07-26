bafae90c348c100c7ca246756d9c9d7bb58894e2
DanielYonzan <adtamang95@gmail.com>
2021-07-26 15:40:43 +0545

    Diff made changes

--- Diff.java
+++ Diff.java
@@ -2,7 +2,10 @@ class Diff {
     public static void main(String[] args) {
         int a = 10;
         int b = 4;
+        int diff = a-b;
 
-        System.out.println("Difference between a and b is: " a-b);
+
+        System.out.println("The diff is :" diff);
     }
 }
+
